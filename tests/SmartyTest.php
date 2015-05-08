1883579c73c3f6e9b918b9e4758ece1ba1d16ae4
Mike Estes <mikejestes@gmail.com>
2015-05-07 17:00:17 -0700

    Get tests to pass style guide.

--- tests/SmartyTest.php
+++ tests/SmartyTest.php
@@ -1,31 +1,32 @@
 <?php
 
-require dirname(__FILE__) . '/../vendor/autoload.php';
+require dirname(__FILE__).'/../vendor/autoload.php';
 
 class Configuration {
-    public static function get($key) {
-        return '';
-    }
+	public static function get($key)
+	{
+		return $key;
+	}
 }
 
 class SmartyTest extends PHPUnit_Framework_TestCase
 {
-    public function testAdminTemplate()
-    {
-        $s = new Smarty();
-        $s->assign('module_dir', '');
-        $s->assign('pw_signup_url', '');
-        $s->setTemplateDir(dirname(__FILE__) . '/../views/templates/');
-        $output = $s->fetch('admin/configure.tpl');
-        $this->assertTrue(strlen($output) > 0);
-    }
+	public function testAdminTemplate()
+	{
+		$s = new Smarty();
+		$s->assign('module_dir', '');
+		$s->assign('pw_signup_url', '');
+		$s->setTemplateDir(dirname(__FILE__).'/../views/templates/');
+		$output = $s->fetch('admin/configure.tpl');
+		$this->assertNotEmpty($output);
+	}
 
-    public function testButtonTemplate()
-    {
-        $s = new Smarty();
-        $s->assign('pw_api_key', '');
-        $s->setTemplateDir(dirname(__FILE__) . '/../views/templates/');
-        $output = $s->fetch('hook/widget.tpl');
-        $this->assertTrue(strlen($output) > 0);
-    }
+	public function testButtonTemplate()
+	{
+		$s = new Smarty();
+		$s->assign('pw_api_key', '');
+		$s->setTemplateDir(dirname(__FILE__).'/../views/templates/');
+		$output = $s->fetch('hook/widget.tpl');
+		$this->assertNotEmpty($output);
+	}
 }
